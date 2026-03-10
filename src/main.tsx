667ce4d0bb88504f855f715f3547a0a9635ed20b
ptmeo <manhptph32121@fpt.edu.vn>
2026-03-10 14:31:41 +0700

    kk

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import "antd/dist/reset.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
